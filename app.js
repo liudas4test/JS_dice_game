975a1192498a5e094a6aed33c2ec92b67d53e4ea
liudas4test <liudas.keturi@outlook.com>
2017-11-19 18:07:12 +0000

    starting over

--- app.js
+++ app.js
@@ -12,23 +12,9 @@ GAME RULES:
 var scores = [0,0];
 var roundScore = 0;
 var activePlayer = 0;
+var dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
-document.querySelector('.btn-roll').addEventListener('click',function(){
-	//1
-	var dice = Math.floor(Math.random() * 6) + 1;
-	//2
-	var diceDOM = document.querySelector('.dice');
-	diceDOM.style.display = 'block';
-	diceDOM.src = 'dice-' + dice + '.png';
-
-	//3
+document.querySelector('#current-0').textContent = dice;
 
 
-	
-
-});
